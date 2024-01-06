2c5d0d62c161acb0962eb33381a0f340e6d45098
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2024-01-07 00:20:22 +0300

    Update __init__.py

--- zira/__init__.py
+++ zira/__init__.py
@@ -25,7 +25,7 @@ StartTime = time.time()
 zedversion = "3.1.1"
 
 if Config.UPSTREAM_REPO == "zel":
-    UPSTREAM_REPO_URL = "https://github.com/Zed-Thon/Zelzal"
+    UPSTREAM_REPO_URL = "https://github.com/abdalaleQ/ZelZal"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 
